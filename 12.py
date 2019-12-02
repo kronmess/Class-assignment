0988a7a2cab3377fda0173d1338778a1ce31df4f
kronmess <57931505+kronmess@users.noreply.github.com>
2019-12-02 09:07:32 +0700

    Update 12.py

--- 12.py
+++ 12.py
@@ -4,7 +4,7 @@ def caesar_cipher(text:str):
     new_word = ""
     for x in text:
         next = alphabet.find(x) + 13
-        if next > 26:
+        if next >= 26:
             next = next-26
         new_letter = alphabet[next]
         new_word = new_word +new_letter
